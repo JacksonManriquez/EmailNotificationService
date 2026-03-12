c52c4724bbceb1a278f8a09978322214d09daa81
JacksonManriquez <pukuku101@gmail.com>
2026-03-11 19:35:14 -0700

    Second round of testing

--- mailservice.py
+++ mailservice.py
@@ -2,18 +2,31 @@ from flask import Flask, request, jsonify
 import smtplib
 from email.mime.text import MIMEText
 
+def load_blacklist():
+    with open("blacklist.txt") as f:
+        return set(line.strip() for line in f)
+    
+
+
 app = Flask(__name__)
 
 EMAIL_ADDRESS = "saltedhampukuku@gmail.com"
 EMAIL_PASSWORD = "oaxm zncs oiau jvhi"
 
 
+
+
 def send_email(to_email, subject, message):
 
+
+    Black_List = load_blacklist()
     msg = MIMEText(message)
     msg['Subject'] = subject
     msg['From'] = EMAIL_ADDRESS
     msg['To'] = to_email
+
+    if to_email in Black_List:
+        return { "error": "Email address blocked"}
 
     try:
         server = smtplib.SMTP("smtp.gmail.com", 587)
@@ -28,11 +41,28 @@ def send_email(to_email, subject, message):
         print("Email error:", e)
         return False
 
+@app.route("/send-email", methods=["POST"])
+def email_endpoint():
 
+    data = request.json
+
+    email = data.get("email")
+    subject = data.get("subject")
+    message = data.get("message")
+
+    if not email or not message:
+        return jsonify({"error": "Missing email or message"}), 400
+
+    success = send_email(email, subject, message)
+
+    if success:
+        return jsonify({"status": "Email sent"})
+    else:
+        return jsonify({"status": "Failed to send email"}), 500
 
 
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=3002, debug=True)
 
--- test_suite.py
+++ test_suite.py
@@ -6,4 +6,4 @@ data = {
     "message": "You have successfully signed in."
 }
 
-requests.post("http://localhost:5001/send-email", json=data)
+requests.post("http://192.168.4.37:3002/send-email", json=data)
